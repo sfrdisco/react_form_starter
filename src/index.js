a646296eba1d80ca57e7afcfd7da641c4feb242f
sfrdisco <shawnfriedel@discover.com>
2022-10-21 10:05:15 +0000

    ok

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { useForm } from "react-hook-form";
+
+import "./styles.css";
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors }
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  }; // your form submit function which will invoke after successful validation
+
+  console.log(watch("example")); // you can watch individual input by pass the name of the input
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* register your input into the hook by invoking the "register" function */}
+      <input defaultValue="test" {...register("example")} />
+
+      {/* include validation with required or other standard HTML validation rules */}
+      <input {...register("exampleRequired", { required: true })} />
+      {/* errors will return when field validation fails  */}
+      {errors.exampleRequired && <p>This field is required</p>}
+
+      <input type="submit" />
+    </form>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
